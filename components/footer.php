16f0eac859df4e27255458d69f11996614306416
FaizAkbar26 <92351023+FaizAkbar26@users.noreply.github.com>
2024-08-02 19:54:56 +0700

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,31 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="index.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us</h3>
+         <a href="wa.me/081362096752"><i class="fas fa-phone"></i> +62 878 3051 4409</a>
+         <a href="mailto:loremipsum@gmail.com"><i class="fas fa-envelope"></i> gheascamping@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Pekanbaru, Riau </a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Pandeyan Team</span> | all rights reserved</div>
+
+</footer>
